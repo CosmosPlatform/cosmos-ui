7a2034d78a8b1c16b816916600974515cf7931a0
Rafael <rafaelberenguel15@gmail.com>
2025-05-10 13:21:27 -0300

    Fix problem with reponsiveness

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function AdminLoginPage() {
   return (
-    <div className="relative container flex h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="relative flex h-screen w-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
