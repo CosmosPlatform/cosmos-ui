b470495ef29c07460c27247edf35380f0f9b0154
Rafael <rafaelberenguel15@gmail.com>
2025-10-12 14:46:35 -0300

    UI-011: Suppresing watning in development

--- src/components/swagger/swagger.tsx
+++ src/components/swagger/swagger.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import "./swagger-custom.css";
@@ -9,6 +10,24 @@ interface SwaggerProps {
 }
 
 export default function Swagger({ spec }: SwaggerProps) {
+  useEffect(() => {
+    // Suppress UNSAFE_componentWillReceiveProps warnings from swagger-ui-react
+    const originalError = console.error;
+    console.error = (...args) => {
+      if (
+        typeof args[0] === "string" &&
+        args[0].includes("UNSAFE_componentWillReceiveProps")
+      ) {
+        return;
+      }
+      originalError.apply(console, args);
+    };
+
+    return () => {
+      console.error = originalError;
+    };
+  }, []);
+
   return (
     <div className="swagger-ui-wrapper">
       <SwaggerUI spec={spec} />
